3191d03c128db25808e0ec01327147c4cac0e1bb
Gopaljha03 <gopaljha1425@gmail.com>
2026-01-06 22:50:35 +0530

    Time: 5 ms (69.22%), Space: 45.7 MB (77.33%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,39 @@
 class Solution {
-    public String minWindow(String s, String t) {
-        if(s.length() < t.length())return "";
-        int [] need = new int[128];
-        for(char c: t.toCharArray()){
+    public String minWindow(String s, String t) { 
+        
+        int[]need = new int[128];
+        for(char c :t.toCharArray()){
             need[c]++;
         }
-        int left = 0, right = 0;
-        int count = t.length();
+        int left = 0;
         int minLen = Integer.MAX_VALUE;
         int start = 0;
+        int missing = t.length();
 
-        while(right < s.length()){
-            char r = s.charAt(right);
-            if(need[r] > 0){
-                count--;
+        for(int right = 0; right<s.length();right++){
+            char c =  s.charAt(right);
+            if(need[c]>0){
+                missing--;
             }
-            need[r]--;
-            right++;
+            need[c]--;
 
-            while(count == 0){
-                if(right - left < minLen){
-                    minLen = right - left;
+            while(missing==0){
+                if(right-left+1<minLen){
+                    minLen = right-left+1;
                     start = left;
                 }
-                char l = s.charAt(left);
-                need[l]++;
-                if(need[l]>0){
-                    count++;
+                char leftchar = s.charAt(left);
+                need[leftchar]++;
+                if(need[leftchar]>0){
+                    missing++;
                 }
                 left++;
+
             }
+            
+            
         }
-        return minLen == Integer.MAX_VALUE ?"" :s.substring(start , start+minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
+        
