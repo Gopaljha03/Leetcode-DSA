476a9d26bd86fc14a0296838d88bd73e47d8d3b8
Gopaljha03 <gopaljha1425@gmail.com>
2026-04-04 09:51:19 +0530

    Time: 5 ms (99.56%), Space: 44.9 MB (95.17%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -3,41 +3,26 @@ import java.util.*;
 class Solution {
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        Arrays.sort(candidates); 
-
-        backtrack(0, candidates, target, new ArrayList<>(), ans);
-
+        List<List<Integer>>ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(0,candidates,target, new ArrayList<>(), ans);
         return ans;
     }
-
-    private void backtrack(int index, int[] arr, int target,
-                           List<Integer> current,
-                           List<List<Integer>> ans) {
-
-        
-        if (target == 0) {
+    public void backtrack(int index, int[]arr, int target,List<Integer>current, List<List<Integer>>ans){
+        if(target == 0){
             ans.add(new ArrayList<>(current));
             return;
         }
+        for(int i = index ; i < arr.length;i++){
+            if(i > index && arr[i] == arr[i-1])continue;
+            if(arr[i]>target)break;
 
-        for (int i = index; i < arr.length; i++) {
-
-           
-            if (i > index && arr[i] == arr[i - 1]) continue;
-
-           
-            if (arr[i] > target) break;
-
-            
             current.add(arr[i]);
 
-           
-            backtrack(i + 1, arr, target - arr[i], current, ans);
-
-           
-            current.remove(current.size() - 1);
+            backtrack(i +1, arr,target-arr[i],current, ans);
+            current.remove(current.size()-1);
         }
     }
+
+   
 }
