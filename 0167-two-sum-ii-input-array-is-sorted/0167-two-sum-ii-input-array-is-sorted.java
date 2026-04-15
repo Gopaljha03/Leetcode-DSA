a8e038b51127a49944c685cedb017b0ab871622c
Gopaljha03 <gopaljha1425@gmail.com>
2026-04-15 11:44:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,24 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+        while(left< right){
+            int sum = numbers[left]+numbers[right];
+            if(sum == target){
+                return new int[]{left+1, right+1};
+            }
+            else if(sum < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+}
+/*class Solution {
+    public int[] twoSum(int[] numbers, int target) {
         int n = numbers.length;
         for(int i = 0; i <n;i++){
             for(int j = i+1; j < n;j++){
@@ -10,4 +29,4 @@ class Solution {
         }
         return new int[]{-1,-1};
     }
-}
+}*/
