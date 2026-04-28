92cb40eb57c6f4de1e7c8fe096b083cf38164af0
Gopaljha03 <gopaljha1425@gmail.com>
2026-04-28 14:21:20 +0530

    Time: 0 ms (100%), Space: 42 MB (54.59%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,32 +1,25 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        for(int i = 1; i<=n;i++){
-            if(i == 1){
-                dp[1] = dp[i-1]+0;
-            }else{
-                dp[i] = dp[i-1]+ dp[i-2];
-            }
-        }
+        int[]dp = new int[n+1];
+        Arrays.fill(dp , -1);
+        return solve(dp, n);
+        
+
+    
+    }
+    public int solve(int[]dp , int n){
+        if(n == 0 || n==1)return 1;
+        if(dp[n] != -1)return dp[n];
+        dp[n] = solve(dp, n-1) + solve(dp, n-2);
         return dp[n];
     }
 }
-//MEMORIZATION
-/*class Solution {
+/*RECURSION
+class Solution {
     public int climbStairs(int n) {
-       int[] ways = new int[n+1];
-       return dfs(n, ways);
-        
-    }
-    private int dfs(int n, int[]ways){
-        if(n == 0) return 1;
-        if(n < 0) return 0;
-
-        if(ways[n] !=0) return ways[n];
-
-        ways[n] = dfs(n-1, ways)+dfs(n-2,ways);
-        return ways[n];
-
+        if(n == 0 || n==1){
+            return 1;
+        }
+        return climbStairs(n-1)+ climbStairs(n-2);
     }
 }*/
