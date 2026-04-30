a2b497ceaac4a4ac0b656e114f068fe8af563fa2
Gopaljha03 <gopaljha1425@gmail.com>
2026-04-30 15:02:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,20 @@
+//SPACE OPTIMIZATION
 class Solution {
-    
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return nums[0];
-        int[]dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-
-        for(int i = 2; i < n;i++){
-            dp[i] = Math.max(dp[i-1],
-            nums[i]+ dp[i-2]
-            );
+        int prev2 = 0;
+        int prev1 = 0;
+        for(int num : nums){
+            int curr = Math.max(prev1, num + prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
     
 }
-/*class Solution {
+/*
+RECURSION
+class Solution {
     public int rob(int[] nums) {
         return solve(nums, nums.length -1);
         
@@ -27,7 +25,9 @@ class Solution {
             solve(nums, i-1), nums[i] + solve(nums, i-2)
         );
     }
-}class Solution {
+}
+MEMOIZAION
+class Solution {
     int[]dp;
     public int rob(int[] nums) {
         int n = nums.length;
@@ -44,6 +44,25 @@ class Solution {
         nums[i]+ solve(nums, i-2)
         );
         return dp[i];
+    }
+    
+}
+TABULATION
+class Solution {
+    
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[]dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+
+        for(int i = 2; i < n;i++){
+            dp[i] = Math.max(dp[i-1],
+            nums[i]+ dp[i-2]
+            );
+        }
+        return dp[n-1];
     }
     
 }
