733c77f2f100ede5f35b8cea20bd1460b67124af
Gopaljha03 <gopaljha1425@gmail.com>
2026-05-07 15:52:36 +0530

    Time: 2571 ms (5.01%), Space: 157.6 MB (10.96%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,6 +2,41 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
+        int [] result = new int[n-k+1];
+        int max = nums[0];
+        for(int i = 0; i <k ;i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        result[0] = max;
+
+        for(int i = 1; i <=n-k ;i++){
+            int outgoing = nums[i-1];
+            int incoming = nums[i+k-1];
+
+            if(outgoing == max){
+                max = nums[i];
+                for(int j = i ; j <i+k;j++){
+                    if(nums[j]> max){
+                        max = nums[j];
+                    }
+                }
+            }else{
+                if(incoming > max){
+                    max = incoming;
+                }
+            }
+            result[i] = max;
+        }
+        return result;
+       
+        
+    }
+}
+/*class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
         int[]result = new int[n-k+1];
         Deque<Integer>dq = new ArrayDeque<>();
         int j = 0;
@@ -25,4 +60,4 @@ class Solution {
        
         
     }
-}
+}*/
