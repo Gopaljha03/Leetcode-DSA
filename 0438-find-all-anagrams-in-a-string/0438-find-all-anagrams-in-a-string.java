7b9b962103c5c272a9cb81034207842683cc9167
Gopaljha03 <gopaljha1425@gmail.com>
2026-05-08 12:16:10 +0530

    Time: 33 ms (28.84%), Space: 47.5 MB (21.33%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,49 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        if(s.length() < p.length())return result;
-        int freqP[] = new int[26];
-        int window[] = new int[26];
-
-        for(char c: p.toCharArray()){
-            freqP[c-'a']++;
+        List<Integer> result =  new ArrayList<>();
+        if(s.length()<p.length())return result;
+        HashMap<Character, Integer>map = new HashMap<>();
+        for(char c :p.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
         }
-        int left = 0;
-        for(int right = 0;right <s.length();right++){
-            window[s.charAt(right)-'a']++;
+        int count =  map.size();
+        int i = 0, j = 0;
+        int k = p.length();
 
-            if(right-left+1>p.length()){
-                window[s.charAt(left)- 'a']--;
-                left++;
+        while(j < s.length()){
+            char ch = s.charAt(j);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)== 0){
+                    count--;
+                }
             }
-            if(right-left+1 == p.length()){
-                if(Arrays.equals(freqP,window)){
-                    result.add(left);
+            if(j-i+1<k){
+                j++;
+            }else if(j - i +1 == k){
+                if(count  == 0){
+                    result.add(i);
+                }
+                char left = s.charAt(i);
+                if(map.containsKey(left)){
+                    map.put(left,map.get(left)+1);
+                    if(map.get(left)== 1)count++;
                 }
                 
-
+                i++;
+                j++;
             }
         }
         return result;
     }
-} 
-
-/*   public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList<>();
-        int [] freqP  = new int[26];
-        for(char c : p.toCharArray()){
-            freqP[c - 'a']++;
-        }
-        for(int i =0; i<=s.length()-p.length();i++){
-            int [] freq = new int[26];
-
-            for(int j = i; j<i+p.length();j++){
-                freq[s.charAt(j) - 'a']++;
-            }
-            if(Arrays.equals(freqP,freq)){
-                result.add(i);
-            }
-        }
-        return result;
-    }
-} */
+}
